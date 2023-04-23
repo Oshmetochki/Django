32593fe546958e2f10c11c32cd4feb646ce71664
oshmetochki <toni.krivorot@gmail.com>
2023-04-23 14:17:25 +0300

    migrations

--- first/first/apps/glows/models.py
+++ first/first/apps/glows/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Glow(models.Model):
+    glow_title=models.CharField('Название статьи',max_length=200)
+    glow_text=models.TextField('Текст статьи')
+    pub_date=models.DateTimeField('дата публикации')
+
+
+class Comment(models.Model):
+    glow=models.ForeignKey(Glow,on_delete=models.CASCADE)
+    author_name=models.CharField('имя автора',max_length=50)
+    comment_text=models.CharField('текст комментария',max_length=200)
